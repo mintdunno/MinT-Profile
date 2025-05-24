de2fb060f3b33c86586c98b5cdafff6a0f823a55
MinT <tnm1042002@gmail.com>
2025-05-24 22:01:25 +0700

    Add initial implementation of area calculation and main function

---
+++ testing.py
@@ -0,0 +1,22 @@
+def calculate_area(length, width
+    area = length * width
+    print("The area is: " + area
+    return area
+
+def main():
+    x = 10
+    y = 5
+    if x > y
+        result = calculate_area(x, y
+        print("Calculation complete"
+    else
+        print("Invalid input")
+    
+    numbers = [1, 2, 3, 4, 5
+    for num in numbers
+        if num % 2 = 0:
+            print(num + " is even")
+        else
+            print(num, "is odd")
+
+main(
